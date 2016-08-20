124d5b644650ba73d7ed8ce42e8570fe5b68ccaf
nandahibatullah <nandahibatullah@gmail.com>
2016-08-21 10:26:14 +1200

    added the map and functionality
    need to load the json and then interate thru it

--- app/static/map.js
+++ app/static/map.js
@@ -1,14 +1,38 @@
 function initMap() {
-    var myLatLng = {lat: -25.363, lng: 131.044};
+    var nzLatLon = {lat: -40.9006, lng: 174.8860};
+    var markers = [];
 
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
-        center: myLatLng
+        center: nzLatLon
     });
 
     var marker = new google.maps.Marker({
-        position: myLatLng,
+        position: nzLatLon,
         map: map,
         title: 'Hello World!'
     });
+
+
+    marker.addListener('click', function() {
+        map.setZoom(8);
+        map.setCenter(marker.getPosition());
+    });
+
+
+    $.getJSON('data.json', function (json) {
+        for (var key in json) {
+            if (json.hasOwnProperty(key)) {
+                var item = json[key];
+                markers.push({
+                    name: item.name,
+                    safe: item.safe,
+                    reason: item.reason,
+                    location: item.location
+                });
+            }
+        }
+    });
+
+
 }
